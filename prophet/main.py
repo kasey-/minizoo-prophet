b4bc4394b27de2ea046025a7ed1b66452db0ffd7
Lucas Fernandez <lucas.fernandez@champ.aero>
2018-12-21 12:35:16 +0100

    Add web client and refacto backend

--- prophet/main.py
+++ prophet/main.py
@@ -6,8 +6,11 @@ from flask_cors import CORS
 from flask import request
 import mimetypes
 import hashlib
+import bz2
 import pickle
 
+import json
+import numpy as np
 import pandas as pd
 from fbprophet import Prophet
 
@@ -15,46 +18,29 @@ app = Flask(__name__)
 CORS(app)
 m = Prophet()
 
-def read_dataset(dataset):
-    if(dataset.content_type == 'text/csv'):
-        return pd.read_csv(dataset)
-    elif(
-          (dataset.content_type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet') or
-          (dataset.content_type == 'application/vnd.ms-excel')
-        ):
-        return pd.read_excel(dataset)
-    else:
-        return pd.DataFrame({'ds':[],'y':[]})
-
-def sanitize_dataset(df):
-    df = df.rename(columns={df.columns[0]:"ds", df.columns[1]:"y"})
-    return df
-
 @app.route('/prophet/dataset', methods=["POST"])
 def dataset():
-    dataset = request.files.get('dataset')
-    df = read_dataset(dataset)
-    df = sanitize_dataset(df)
-    fileid = hashlib.sha224(dataset.filename.encode('utf-8')).hexdigest()
-    df.to_pickle("./files/"+fileid+".pkl.compress", compression="gzip")
-    if df.empty:
-        return "Error at dataset importation"
-    return '{"id":"'+fileid+'"}'
+    fileid = hashlib.sha224(str(request.data).encode('utf-8')).hexdigest()
+    dataFromJson = json.loads(request.data)
+    dataNPArray = np.array(dataFromJson)
+    df = pd.DataFrame({'ds':dataNPArray[:,0],'y':dataNPArray[:,1]})
+    cfile = bz2.BZ2File("./files/"+fileid+".pkl.bz2", 'w')
+    pickle.dump(df, cfile)
+    return json.dumps({"id":fileid,"import":True})
 
 @app.route('/prophet/<fileid>/fit', methods=["POST"])
 def fit(fileid):
-    df = pd.read_pickle("./files/"+fileid+".pkl.compress", compression="gzip")
+    df = pd.read_pickle("./files/"+fileid+".pkl.bz2", compression="bz2")
     m = Prophet()
     m.fit(df)
-    with open("./models/"+fileid+".pkl", "wb") as f:
-        pickle.dump(m, f)
-    return '{"id":"'+fileid+'","fit":True}'
+    cfile = bz2.BZ2File("./models/"+fileid+".pkl.bz2", 'w')
+    pickle.dump(m, cfile)
+    return json.dumps({"id":fileid,"fit":True})
 
 @app.route('/prophet/<fileid>/predict/<int:periods>', methods=["GET"])
 def predict(fileid,periods):
-    m = pickle.load(open("./models/"+fileid+".pkl", "rb"))
+    m = pickle.load(bz2.BZ2File("./models/"+fileid+".pkl.bz2", "r"))
     future = m.make_future_dataframe(periods=periods)
-    print(future.tail())
     forecast = m.predict(future)
     print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail())
     return "prophet predict: "+periods
