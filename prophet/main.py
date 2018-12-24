fb0c8df2ac2959b860a581c359a983c63c1d48ab
Lucas Fernandez <contact@kasey.fr>
2018-12-24 17:59:00 +0100

    First 'working' code

--- prophet/main.py
+++ prophet/main.py
@@ -4,10 +4,12 @@
 from flask import Flask
 from flask_cors import CORS
 from flask import request
+from flask import jsonify
 import mimetypes
 import hashlib
 import bz2
 import pickle
+import lttb
 
 import json
 import numpy as np
@@ -21,7 +23,7 @@ m = Prophet()
 @app.route('/prophet/dataset', methods=["POST"])
 def dataset():
     fileid = hashlib.sha224(str(request.data).encode('utf-8')).hexdigest()
-    dataFromJson = json.loads(request.data)
+    dataFromJson = request.get_json()
     dataFromJson.pop()
     dataNPArray = np.array(dataFromJson)
     df = pd.DataFrame({'ds':dataNPArray[:,0],'y':dataNPArray[:,1]})
@@ -31,15 +33,17 @@ def dataset():
     modelArchive = bz2.BZ2File("./archive/"+fileid+".model.bz2", 'w')
     pickle.dump(df, dataArchive)
     pickle.dump(m, modelArchive)
-    return json.dumps({"id":fileid,"import":True,"fit":True})
+    return jsonify({"id":fileid,"import":True,"fit":True})
 
 @app.route('/prophet/<fileid>/predict/<int:periods>', methods=["GET"])
 def predict(fileid,periods):
     m = pickle.load(bz2.BZ2File("./archive/"+fileid+".model.bz2", "r"))
     future = m.make_future_dataframe(periods=periods)
     forecast = m.predict(future)
-    print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail())
-    return '{"id":fileid,"forecast":'+forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].to_json()+'}'
+    predict = json.loads(
+        forecast[['ds','yhat','yhat_lower','yhat_upper']].to_json()
+    ) # json coding decoding ugly hack ...
+    return jsonify({"id":fileid,"forecast":predict})
 
 if __name__ == '__main__':
     app.run(debug=True)
